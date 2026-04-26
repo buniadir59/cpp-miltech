201f149dbca734fe066f0856910f8c9a7c57a735
Olga Dmytrenko <buniadir@gmail.com>
2026-04-26 11:52:26 +0000

    added wheel odometry formulas

--- homework_04/src/main.cpp
+++ homework_04/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <ostream>
 #include <string>
@@ -21,19 +22,32 @@
 constexpr long kTicksPerRevolution = 1024;  //iмпульсiв на один оберт колеса
 constexpr double kWheelRadiusM = 0.3;       //радiус колеса у метрах (дiаметр 60 см)
 constexpr double kWheelbaseM = 1.0;         //вiдстань мiж лiвим i правим бортом, у метрах
-
+constexpr double kDistancePerTick = 2.0 * M_PI * kWheelRadiusM / kTicksPerRevolution;
 
 struct position_t {
     double x=0, y=0, theta=0;
 };
 
 std::ostream& operator<<(std::ostream& os, const position_t& p) { //overloaded << for position_t
-    return os << p.x << " " << p.y << " " << p.theta;
+    return os << std::fixed << std::setprecision(2) << p.x << " " << p.y << " " << p.theta;
 }
  
 struct odo_t {
     long fl_ticks, fr_ticks, bl_ticks, br_ticks;
+
+    odo_t& operator-=(const odo_t& delta) {  //decrement
+        this->fl_ticks -= delta.fl_ticks;
+        this->fr_ticks -= delta.fr_ticks;
+        this->bl_ticks -= delta.bl_ticks;
+        this->br_ticks -= delta.br_ticks;
+        return *this;
+    }
 };
+
+odo_t operator-(odo_t a1, const odo_t& a2) { //diff
+        a1 -= a2;
+        return a1;
+}
 
 std::ostream& operator<<(std::ostream& os, const odo_t& o) { //overloaded << for tick_t
     return os << "F(" << o.fl_ticks << ", " << o.fr_ticks
@@ -83,11 +97,24 @@ int main(int argc, char** argv) {
             continue;
         }
 
+        // calculate delta from previous -> current
+        odo_t delta = current - previous; 
 
-        // TODO: calculate delta from previous -> current
-        // update pos
+        //Усереднити борти (передне i заднє колесо одного боку обертаються синхронно)&Перевести у метри:
+        double dl = (kDistancePerTick / 2.0) * (delta.fl_ticks + delta.bl_ticks);
+        double dr = (kDistancePerTick / 2.0) * (delta.fr_ticks + delta.br_ticks);
 
-        std::cout << timestamp_ms << ' ' << pos << '\n'; //show position
+        //Скiльки пройшов центр робота i на скiльки повернувся:
+        double dist  = (dl + dr) / 2.0; //d = (dL + dR) / 2 // пройдена вiдстань центру
+        double dtheta = (dr - dl) / kWheelbaseM; //dtheta = (dR - dL) / wheelbase_m // змiна орiєнтацiї
+
+        // update pos: midpoint integration (усереднений напрямок на кроцi)
+        pos.x += dist * cos(pos.theta + dtheta / 2.0);
+        pos.y += dist * sin(pos.theta + dtheta / 2.0);
+        pos.theta += dtheta;
+
+        //show position
+        std::cout << timestamp_ms << ' ' << pos << '\n'; 
 
         previous = current;
     }
