78feb847ab815b7bf54dfc8495e8cccca10486e0
Olga Dmytrenko <buniadir@gmail.com>
2026-05-05 17:51:44 +0000

    Added validation of input fields in parse_frame() and validation of frames flow in read_frames()

--- homework_05/src/telemetry.cpp
+++ homework_05/src/telemetry.cpp
@@ -6,6 +6,7 @@
 #include <limits>
 #include <fstream>
 #include <iostream>
+#include <string>
 
 
 // Debugging exercise notes:
@@ -48,7 +49,7 @@ bool parse_long(const char* text, int line_count, long& res_long) {
     res_long = std::strtol(text, &end, 10);
 
     if (errno == ERANGE || end == text  || *end != '\0') { 
-        std::cerr   << "error: invalid integer value at line " << line_count << ".\n";
+        std::cerr   << "ERR: invalid integer value at line " << line_count << ".\n";
         return false;
     }
 
@@ -63,7 +64,7 @@ bool parse_int(const char* text, int line_count, int& res_int) {
 
     if (res_long < std::numeric_limits<int>::min()
              || res_long > std::numeric_limits<int>::max()) {
-        std::cerr << "error: integer value out of range at line " << line_count << "\n";             
+        std::cerr << "ERR: integer value out of range at line " << line_count << "\n";             
         return false;
     }
 
@@ -77,20 +78,32 @@ bool parse_double(const char* text, int line_count, double& res_double) {
     res_double = std::strtod(text, &end);
 
     if (errno == ERANGE ||end == text || *end != '\0') { //
-        std::cerr   << "error: invalid double value at line " << line_count << ".\n";
+        std::cerr   << "ERR: invalid double value at line " << line_count << ".\n";
         return false;
     }
 
     return true;
 }
 
+/****
+ * parses frame in line, returns result (true/false) and validated frame 
+ * validation includes:
+ *  - number of fields must be EXPECTED_FIELD_COUNT
+ *  - voltage_v > 0
+ *  - temperature_c in [-40, 120] range 
+ *  - gps_fix = 0 or 1
+ *  - satellites >= 0
+ * in case of parsing or validation error, error message is generated
+ * with detailed error description and line number with erroneus input
+ */ 
 bool parse_frame(char line[], int line_count, Frame& frame) {
     char* fields[EXPECTED_FIELD_COUNT] = {};
     const int field_count = split_line(line, fields, EXPECTED_FIELD_COUNT);
+    const std::string errhead = "ERR: invalid input data in line ";
 
     if (field_count != EXPECTED_FIELD_COUNT) { //invalid data in line
-        std::cerr   << "error: invalid input data in line " << line_count 
-                    << ". Expected "<< EXPECTED_FIELD_COUNT << " fields.\n";
+        std::cerr   << errhead << line_count << ": expected "
+                    << EXPECTED_FIELD_COUNT << " fields.\n";
         return false;
     }
 
@@ -103,38 +116,92 @@ bool parse_frame(char line[], int line_count, Frame& frame) {
         || !parse_int(fields[5], line_count, frame.gps_fix)
         || !parse_int(fields[6], line_count, frame.satellites) 
     ) return false;
-        
+
+    if (frame.voltage_v <= 0) {
+        std::cerr   << errhead << line_count << ": voltage_v must be > 0.\n";
+        return false;  
+    }
+
+    if (frame.satellites < 0) {
+        std::cerr   << errhead << line_count << ": satellites must be >=0.\n";
+        return false;  
+    }
+
+    if (!(frame.gps_fix == 0 || frame.gps_fix == 1)) {
+        std::cerr   << errhead << line_count << ": gps_fix must be 0 or 1.\n";
+        return false;  
+    }
+
+    if ((frame.temperature_c < -40.0 || frame.temperature_c >= 120)) {
+        std::cerr   << errhead << line_count 
+                    << ": temperature_c must be in [-40, 120] range.\n";
+        return false;  
+    }
+
     return true;
 }
 
-double compute_frame_rate_hz(const Frame frames[], int frame_count) {
-    const long elapsed_ms = frames[frame_count - 1].timestamp_ms - frames[0].timestamp_ms;
+/****
+ *   returns frame frequency in Hz (double)
+ *   if frame frequency cannot be calculated, returns 0
+ */
+double compute_frame_rate_hz(const Frame frames[], int frame_count) { 
+    //defensive check
+    if (frame_count < 2) {
+        return 0.0;
+    }
 
-    return static_cast<double>((frame_count - 1) * 1000 / elapsed_ms);
+    const long elapsed_ms = frames[frame_count - 1].timestamp_ms - frames[0].timestamp_ms;
+    if (elapsed_ms <= 0) {
+        return 0.0;
+    }
+
+    return static_cast<double>(frame_count - 1) * 1000.0 / static_cast<double>(elapsed_ms);
 }
 
+/****
+ * reads frames from text file path, not more than max_frames,
+ * puts frames in array frames and validates input
+ * returns number of read frames
+ * generates error message if input file cannot be open, or validation fails
+ * validation includes:
+ *  - timestamp_ms increases every frame
+ *  - seq increments every frame
+ */ 
 int read_frames(const char* path, Frame frames[], int max_frames) {
     std::ifstream input{path};
     if (!input) {
-        std::cerr << "error: failed to open input file: " << path << '\n';
+        std::cerr << "ERR: failed to open input file: " << path << '\n';
         return -1; //fixed return for file open error 
     }
 
     int frame_count = 0;
     int line_count = 0;
     char line[MAX_LINE_LENGTH];
+    const std::string errhead = "ERR: invalid frame at line ";
 
     while (input.getline(line, MAX_LINE_LENGTH)) {
         line_count++;
-        if (line[0] == '\0') {
+        if (line[0] == '\0') { //skip empty line
             continue;
         }
 
         if (frame_count < max_frames) {
             if (parse_frame(line, line_count, frames[frame_count])) {
+                if (frame_count > 0) {
+                    if (frames[frame_count].timestamp_ms <= frames[frame_count - 1].timestamp_ms) {
+                        std::cerr << errhead << line_count << ": timestamp_ms must increase\n";           
+                        return -1;
+                    }
+
+                    if (frames[frame_count].seq - frames[frame_count - 1].seq != 1) {
+                        std::cerr << errhead << line_count << ": seq must increment\n";           
+                        return -1;
+                    }
+                }                
                 ++frame_count;
-            } else {
-                return -1;  //return for invalid input data
+            } else { //err invalid input data
+                return -1;  
             }           
         }
     }
